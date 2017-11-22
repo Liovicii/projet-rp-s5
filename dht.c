9372efad5e3dc25f75dba5118f750a78ae8fe751
Lionel <liovicii@outlook.com>
2017-11-22 15:01:51 +0100

    commit general

--- dht.c
+++ dht.c
@@ -1,5 +1,36 @@
 #include "dht.h"
 
-int port_valide(int port){
-    return port;
+
+int port_valide(char * port){
+	int p = atoi(port);
+    if( (p > 65536) || (p <= 0) ){
+	    return ERROR;
+    }
+    return p;
+}
+
+
+int parse_hostname(char * hostname, char * ip){
+	struct addrinfo hints, *result, *p;
+	struct sockaddr_in6 *h;
+    int s;
+	
+	memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET6;         /* Allow IPv6 */
+    hints.ai_socktype = SOCK_DGRAM;     /* Datagram socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = IPPROTO_UDP;    /* Protocol UDP */
+
+    s = getaddrinfo(hostname, "http", &hints, &result);
+    if(s != 0){
+        return ERROR;
+    }
+    
+    for(p = result; p != NULL; p = p->ai_next){
+ 		h = (struct sockaddr_in6 *) p->ai_addr;
+ 		strcpy(ip, inet_ntoa(h->sin6_addr));	
+    }
+    
+    freeaddrinfo(result);
+    return 0;
 }
--- dht.h
+++ dht.h
@@ -9,8 +9,18 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
 
-#define ERROR -1
+
+#define ERROR       -1
+#define IPV6_LENGTH 100
+
+
+/* Définition des types pour les messages */
+#define GET 1
+#define SET 2
+
 
 /**
  * \def PERRORMSG(cmd, msg)
@@ -24,13 +34,27 @@
 
 
 /**
- * \fn Bool port_valide (int port)
+ * \fn int port_valide (int port)
  * \brief Vérifie si un numéro de port est valide.
  *
- * \param port Un numéro de port.
- * \return TRUE si le paramètre est un numéro de port valide, FALSE sinon.
+ * \param port Un numéro de port sous forme de string.
+ * \return Le numero de port si le paramètre est un numéro de port valide, -1 sinon.
  */
-int port_valide(int port);
+int port_valide(char * port);
+
+
+
+/**
+ * \fn int parse_hostname (char * hostname, char * ip)
+ * \brief Détermine une adresse IPv6 pour un nom de hôte.
+ *
+ * \param hostname Un nom de hôte
+ * \param ip Pointeur où l'on stock l'adresse IP
+ * \return 0 si tout se passe bien, -1 sinon.
+ */
+int parse_hostname(char * hostname, char * ip);
+
+
 
 
 #endif
